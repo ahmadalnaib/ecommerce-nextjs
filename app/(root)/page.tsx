e5385f2cb28948ee1a2ddc99d6e211c721635c24
ahmedhabeeb73 <alnaib888@gmail.com>
2025-02-10 21:43:36 +0100

    add fake data

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,11 @@
-
+import ProductList from '@/components/shared/product/product-list';
+import sampleData from '@/db/sample-data';
 
 const Homepage = () => {
-  return ( <>Store</> );
+  return ( <>
+    <ProductList data={sampleData.products} title="Featured Products" />
+    
+  </> );
 }
  
 export default Homepage;
